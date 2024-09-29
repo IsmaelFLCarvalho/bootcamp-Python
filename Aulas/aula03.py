4eeae000d8cc231f695dfebb62b9af0eac759fdf
Ismael Carvalho <ismaelcarvalhopt@gmail.com>
2024-09-29 15:46:30 +0100

    aulas 3 e 4

---
+++ Aulas/aula03.py
@@ -0,0 +1,15 @@
+#convertendo número inteiro em float
+
+numero_inteiro = 20 #valor inteiro
+numero_inteiro = float(numero_inteiro) #float para converter em float
+print(numero_inteiro) #print com int convertido em inteiro
+
+#convertendo número float para inteiro
+
+numero_float = 10.4 #valor em float
+numero_float = int(numero_float) #int para converter em inteiro
+print(numero_float) #print com float convertido em inteiro
+
+numero_para_string = 15.4
+numero_para_string = str(numero_para_string)
+print(numero_para_string)
---
+++ Aulas/aula04.py
@@ -0,0 +1,5 @@
+# função input
+
+qual_e_o_string = input('informe seu nome: ') #coleta o dado que o usuário irá inserir.
+#depois que o usuário insere um valor no retorno, ele associa à variável. 
+print(f'Seu nome é {qual_e_o_string}.') #printa com a variável já associada ao dado informado pelo usuário.

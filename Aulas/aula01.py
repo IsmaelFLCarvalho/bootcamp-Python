7e7ad003b018df1403d36629901bfb79b838416e
Ismael Carvalho <ismaelcarvalhopt@gmail.com>
2024-09-29 11:05:50 +0100

    frist class python

---
+++ Aulas/aula01.py
@@ -0,0 +1,5 @@
+print("Hello World!") # string
+print(11-2) # int
+print (11.2 -3.1) # float
+print(True) #boolean
+print(False) #boolean
---
+++ Aulas/aula02.py
@@ -0,0 +1,16 @@
+#variáveis e constantes
+name = 'Ismael Carvalho'
+age = 30
+print(f'My name is {name}, and i have {age} years old.')
+
+name, age= 'Jhersica Fernandes', 33
+print(f'My name is {name}, and i have {age} years old.')
+
+
+#constante, python entretanto há uma convenção que uma constante é escrita toda em maiúculo.
+PI =  3.14
+print(PI)
+
+LIMITY_DIARY_SAC = '€200 PER DAY'
+print(LIMITY_DIARY_SAC)
+
